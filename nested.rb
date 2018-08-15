74fa1ad6d02c215658f6479d8a699b9f1f99a4af
Camille Rheaume-Tremblay <camillerheaumet@hotmail.com>
2018-08-15 14:25:27 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -87,7 +87,7 @@ def adding_matz
         }
      }
 programmer_hash[:yukihiro_matsumoto] = :known_for{}
-programmer_hash[:yukihiro_matsumoto] = :languages[]
+#programmer_hash[:yukihiro_matsumoto] = :languages[]
 end
 
 def changing_alan
